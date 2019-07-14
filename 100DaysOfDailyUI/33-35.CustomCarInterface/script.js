af0161cfe70bcf05616fa3cfd4a17dadf428c691
Eleftheria <eleftheriabatsou@gmail.com>
2019-07-14 10:22:10 +0300

    day33 to 36 - Combining projects - DailyUI

---
+++ 100DaysOfDailyUI/33-35.CustomCarInterface/script.js
@@ -0,0 +1,37 @@
+angular.module('MyApp', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
+
+.controller('AppCtrl', function($scope, $mdDialog) {
+    this.myDate = new Date();
+  
+   $scope.showAdvanced = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'dialog1.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+  };
+  
+   function DialogController($scope, $mdDialog) {
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+  }
+ 
+  
+});
